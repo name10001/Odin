ee89375f1a45b98da600985ac253d4925504864a
Isaac Irvine <isaac564465@gmail.com>
2019-04-25 17:04:16 +1200

    If no cards can be played it now ignores the pickup limit

--- Odin/cards/deck.py
+++ Odin/cards/deck.py
@@ -19,21 +19,23 @@ class Deck:
 
         self.set_cards(cards.all_cards.copy())
     
-    def get_weights(self, card_collection=None):
+    def get_weights(self, card_collection=None, ignore_limit=False):
         """
         Calculates the weights for all the cards. Banned cards will be given a probability of 0
         :param card_collection: The card_collection to calculate the weights for.
         If its None, it will calculate the weights for a medium hand
         :return: an array of card weights. The order is the same as Deck.cards
+        :param ignore_limit: Should the card limit be ignored
         I.e. weights[0] is the weight for Deck.cards[0]
         """
-        return [self.get_weight(card, card_collection) for card in self.cards]
+        return [self.get_weight(card, card_collection, ignore_limit=ignore_limit) for card in self.cards]
 
-    def get_weight(self, card, card_collection=None):
+    def get_weight(self, card, card_collection=None, ignore_limit=False):
         """
         Calculates the weights for the given card. Banned cards will be given a probability of 0
         :param card_collection: The card_collection to calculate the weights for.
         If its None, it will calculate the weights for a medium hand
+        :param ignore_limit: Should the card limit be ignored
         :return: The weight of the given card.
         """
         if card_collection is None:
@@ -44,15 +46,18 @@ class Deck:
             else:
                 n_cards = len(card_collection)
                 n_this_type = card_collection.number_of_type(card.CARD_TYPE)
-                return card.CARD_FREQUENCY.get_frequency(n_cards, n_this_type)
+                return card.CARD_FREQUENCY.get_frequency(n_cards, n_this_type, ignore_limit=ignore_limit)
 
     def set_cards(self, cards):
         """
         Set the list of all cards
         Used when undoing a genocide card or initialization
-        :param cards: Card list to pickup future cards from
+        :param cards: card class list to pickup future cards from
         :return: None
         """
+        if len(cards) == 0:
+            raise ValueError("There are no cards")
+
         self.cards = list(cards)
 
         self.all_types.clear()
@@ -67,18 +72,18 @@ class Deck:
             self.all_colours[card.CARD_COLOUR].add(i)
             i += 1
 
-        self.unbanned_colours = self.all_colours.copy()
-        self.banned_colours.clear()
-        self.unbanned_types = self.all_types.copy()
-        self.banned_types.clear()
+        self.unban_all()
 
     def get_random_card(self, card_weights):
         """
         Gets a new random card and returns it.
         :param card_weights: An array of the weights of the cards
-        :return: A Card
+        :return: A card class or None if all the weights are zero
         """
-        return random.choices(self.cards, weights=card_weights)[0]
+        try:
+            return random.choices(self.cards, weights=card_weights)[0]
+        except IndexError:
+            return None
 
     def add_random_cards_to(self, card_collection, number):
         """
@@ -88,12 +93,23 @@ class Deck:
         :return: The list of cards added
         """
         added_cards = []
+        ignore_limit = False
         # calculate weights
-        card_weights = self.get_weights(card_collection=card_collection)
+        card_weights = self.get_weights(card_collection=card_collection, ignore_limit=ignore_limit)
 
-        for i in range(number):
+        picked_up = 0
+        while picked_up < number:
             # get a random card based upon the weights
             card_class = self.get_random_card(card_weights)
+
+            # If it can't pick up anything, ignore the pickup limit, if it still cant pickup anything, unban all cards
+            # This should only happen if all the cards have been banned or all the unbaned cards are at there limit
+            if card_class is None:
+                if ignore_limit is False:
+                    ignore_limit = True
+                else:
+                    self.unban_all()
+                continue
 
             # add card to card_collection
             card = card_class(self.game)
@@ -102,8 +118,10 @@ class Deck:
 
             # update weights of cards with the same type as the one that was picked up
             for index in self.all_types[card_class.CARD_TYPE]:
-                card_weights[index] = self.get_weight(self.cards[index], card_collection)
-        
+                card_weights[index] = self.get_weight(self.cards[index], card_collection, ignore_limit=ignore_limit)
+
+            picked_up += 1
+
         return added_cards
 
     def ban_colour(self, card_colour):
@@ -153,6 +171,16 @@ class Deck:
             self.unbanned_types.add(card_type)
         else:
             raise ValueError("that is not a valid card type")
+
+    def unban_all(self):
+        """
+        Unbans all colours and types
+        :return: None
+        """
+        self.unbanned_colours = self.all_colours.copy()
+        self.banned_colours.clear()
+        self.unbanned_types = self.all_types.copy()
+        self.banned_types.clear()
 
     def get_unbanned_types(self):
         """
